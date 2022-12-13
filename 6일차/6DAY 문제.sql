ed89aabc054bc131cc3f863efa555a75c9faf321
Jiyoon Choi <dasomcjy@naver.com>
2022-12-13 18:32:09 +0900

    12/13 배운내용 커밋

--- 6일차/6DAY 문제.sql
+++ 6일차/6DAY 문제.sql
@@ -60,14 +60,24 @@ from employee e inner join department d
 on e.dno = d.dno 
 where e.dno = 10;
 
+--만약 USING을 사용하면 공통 컬럼 넣어주면 됨
+
+select ename 사원이름, dname 부서이름, loc 지역명 , dno 부서번호 ,job
+from employee e inner join department d      
+using (dno) 
+where dno = 10;
+
+
 --4. NATURAL JOIN을 사용하여 커밋션을 받는 모든 사원의 이름, 부서이름, 지역명을 출력 하시오. 
 
 select  ename 사원이름 , dname 부서이름, loc 지역명 , commission 커밋션
 from employee e NATURAL JOIN department d
 where commission != 0 ;
 
-
+select  ename 사원이름 , dname 부서이름, loc 지역명 , commission 커밋션
+from employee e NATURAL JOIN department d
 where commission is not null;  -- 0인 것까지 출력됨
+
 --5. EQUI 조인과 WildCard ( _ , %)를 사용하여 이름에 A 가 포함된 모든 사원의 이름과 부서명을 출력 하시오. 
 select ename 사원이름, dname 부서명
 from employee e , department d
@@ -88,18 +98,59 @@ where loc = 'NEW YORK';
  
 select e.ename 사원이름, e.eno 사원번호 , e.manager 직속관리자번호, m.eno 직속관리자의사원번호 , m.ename 직속상관이름 
 from employee e , employee m   
-where e.manager = m.eno    
+where e.manager = m.eno  
+
+
+--
+
+select e.ename 사원이름, e.eno 사원번호, m.eno 관리자사번, m.ename 관리자이름
+from employee e join employee m
+on e.manager = m.eno;
+
+
+select eno, ename, manager, eno, ename , manager
+from employee;
+
+select * from employee ;
+
+
     
 --8. OUTER JOIN, SELF JOIN을 사용하여 관리자가 없는 사원을 포함하여 사원번호를 기준으로 내림차순 정렬하여 출력 하시오. 
 select e.eno 사원번호, e.ename 사원이름,  m.eno 직속관리자의사원번호 , m.ename 직속상관이름 
 from employee e, employee m
 where e.manager = m.eno (+)  
-order by e.eno 
+order by e.eno desc;
 
 select e.eno 사원번호, e.ename 사원이름,  m.eno 직속관리자의사원번호 , m.ename 직속상관이름 , e.dno, m.dno
 from employee e, employee m
 where e.manager = m.eno (+)  
-order by e.eno
+order by e.eno desc;
+
+
+---
+-- ANSI 호환
+select e.ename 사원이름, e.eno 사원번호, m.eno 관리자사번, m.ename 관리자이름
+from employee e left outer join employee m
+on e.manager = m.eno
+order by e.eno desc;
+
+select e.ename 사원이름, e.eno 사원번호, m.eno 관리자사번, m.ename 관리자이름
+from employee e right outer join employee m
+on e.manager = m.eno
+order by e.eno desc;
+
+select e.ename 사원이름, e.eno 사원번호, m.eno 관리자사번, m.ename 관리자이름
+from employee e join employee m
+on e.manager = m.eno
+order by e.eno desc;
+
+-- EQUI JOIN 으로 처리 
+
+select e.ename 사원이름, e.eno 사원번호, m.eno 관리자사번, m.ename 관리자이름
+from employee e , employee m
+where e.manager = m.eno (+)
+order by e.eno desc;
+
 
 --9. SELF JOIN을 사용하여 지정한 사원의 이름, 부서번호, 지정한 사원과 동일한 부서에서 근무하는 사원을 출력하시오. 
    --단, 각 열의 별칭은 이름, 부서번호, 동료로 하시오. SCOTT
@@ -107,6 +158,13 @@ order by e.eno
 select e.ename 이름, e.dno 부서번호 , m.ename 동료
 from employee e , employee m
 where (e.dno = m.dno) and (e.ename = 'SCOTT');
+
+--
+select e.ename 이름, e.dno 부서번호, m.ename 동료
+from employee e join employee m
+on e.dno = m.dno
+where e.ename = 'SCOTT';
+   
 
 select *from employee
 
@@ -119,7 +177,8 @@ where (e.manager = m.eno) and (e.dno = m.dno)
 
 select e.ename 사원이름 , e.hiredate 입사일
 from employee e, employee m
-where (e.dno = m.dno) and (e.hiredate >= 'WARD') 
+where (e.ename = m.ename) and (e.hiredate > (select hiredate from employee where ename = 'WARD'))
+
 
 select e.ename 사원이름 , m.ename , e.hiredate 입사일 , m.hiredate
 from employee e, employee m
@@ -133,14 +192,23 @@ where e.hiredate >= '81/02/22'
     --단, 각 열의 별칭을 한글로 넣어서 출력 하시오. 
     select e.ename 사원이름 , e.hiredate 입사일, m.ename 관리자이름, m.hiredate 입사일
     from employee e, employee m
-    where e.job = m.job
+    where (e.ename = m.ename) and (e.hiredate < all (select hiredate from employee
+    where job = 'MANAGER'))
     
     select ename 사원이름, hiredate 입사일 , job
     from employee
     where hiredate < all (select  hiredate from employee
     where job = 'MANAGER');
     
+--    
+
+
+select e.ename 이름, e.hiredate 입사일, m.ename 관리자이름, m.hiredate 관리자입사일
+from employee e join employee m
+on e.manager = m.eno
+where e.hiredate < m.hiredate;    
     
+select eno, ename, hiredate , manager , eno, ename, hiredate, manager from employee;     
     
     
     
@@ -148,17 +216,22 @@ where e.hiredate >= '81/02/22'
 --1. 사원번호가 7788인 사원과 담당 업무가 같은 사원을 표시(사원이름 과 담당업무) 하시오.  
 select ename 사원이름 , job 담당업무
 from employee
-where job = (select job from employee where eno = 7788)
+where job = (select job from employee where eno = '7788')
 
 --2. 사원번호가 7499인 사원보다 급여가 많은 사원을 표시 (사원이름 과 담당업무) 하시오. 
 select ename 사원이름 , job 담당업무 , salary
 from employee
-where salary > (select salary from employee where eno = 7499)
+where salary > (select salary from employee where eno = '7499')
 
 --3. 최소 급여를 받는 사원의 이름, 담당 업무 및 급여를 표시 하시오(그룹 함수 사용)
-select ename 사원이름 , job 담당업무 , salary
+select ename 사원이름 , job 담당업무 , salary 급여
 from employee
 where salary = (select min(salary) from employee)
+
+--
+select ename 사원이름 , job 담당업무 , salary 급여
+from employee
+where salary in(select min(salary) from employee)
 
 --4. 평균 급여가 적은 사원의 담당 업무를 찾아 직급과 평균 급여를 표시하시오.
 
@@ -167,12 +240,31 @@ from employee
 where  salary < (select round( avg(salary)) from employee)
 
 
+--
+
+select min(salary) 급여, job 직급
+from employee
+group by job
+having avg(salary)  <= all (select avg(salary)
+                            from employee
+                            group by job);
+                            
+                            
+select round(avg(salary)) 급여, job 직급
+from employee
+group by job
+having round(avg(salary))  <= all (select round(avg(salary))
+                            from employee
+                            group by job);
+
 --5. 각 부서의 초소 급여를 받는 사원의 이름, 급여, 부서번호를 표시하시오.]
 select  ename 사원이름 , salary 급여, dno 부서번호
 from employee
 where salary in (select  min(salary)
                 from employee
                 group by dno )
+                
+
 
 --6. 담당 업무가 분석가(ANALYST) 인 사원보다 급여가 적으면서 업무가 분석가가 아닌 사원들을 표시 (사원번호, 이름, 담당업무, 급여) 하시오. ALL
 select eno 사원번호, ename 이름, job 담당업무, salary 급여
@@ -186,7 +278,21 @@ select ename 이름
 from employee
 where eno = all (select Manager from employee)
 
+
+--
+select ename
+from employee
+where eno in (select m.eno
+              from employee e right outer join employee m
+              on e.manager = m.eno
+              where e.eno is null);     
+              
 --8. 부하직원이 있는 사원의 이름을 표시 하시오. 
+
+select ename
+from employee
+where eno in (select distinct manager
+              from employee);
 
 
 --9. BLAKE 와 동일한 부서에 속한 사원의 이름과 입사일을 표시하는 질의를 작성하시오(단, BLAKE 는 제외). 
@@ -207,12 +313,21 @@ select eno 사원번호 , ename 사원이름 , dno
 from employee
 where dno in (select dno from employee where ename like '%K%')
 
+
+
 --12. 부서 위치가 DALLAS 인 사원의 이름과 부서 번호 및 담당 업무를 표시하시오. 
 
  select ename 사원이름 , loc 부서위치, job 담당업무, e.dno 부서번호
  from employee e , department d 
  where e.dno = d.dno
  and loc = 'DALLAS';
+ 
+ --
+select ename 사원이름, dno 부서번호, job 담당업무
+from employee
+where dno in (select dno
+              from department
+              where loc = 'DALLAS');
  
 select * from department
 
@@ -227,6 +342,14 @@ where manager = (select eno from employee where ename = 'KING')
  from employee e , department d 
  where e.dno = d.dno
  and dname = 'RESEARCH';
+ 
+ --
+ 
+ select dno 부서번호, ename 사원이름, job 담당업무
+from employee
+where dno = (select dno
+            from department
+            where dname = 'RESEARCH');
 
 --15. 평균 급여보다 많은 급여를 받고 이름에 M이 포함된 사원과 같은 부서에서 근무하는 사원의 사원번호, 이름, 급여를 표시하시오. 
 select eno 사원번호, ename 이름, salary 급여
@@ -235,11 +358,31 @@ where dno in (select dno from employee where (ename like '%M%') )
  
 salary > round(avg(salary))) 
 
+
+--
+select eno 사원번호, ename 사원이름, salary 급여
+from employee
+where salary > (select avg(salary)
+                from employee)
+and dno in (select dno
+           from employee
+           where ename like '%M%');
+
 --16. 평균 급여가 가장 적은 업무를 찾으시오. 
 
 select job 업무 , salary
 from employee
 where Salary <= all(select round(avg(Salary)) from employee group by job)
+
+
+--
+
+select job 업무
+from employee
+group by job
+having avg(salary) <= all (select avg(salary)
+                          from employee
+                          group by job);
 
 --17. 담당업무가 MANAGER인 사원이 소속된 부서와 동일한 부서의 사원을 표시하시오.     
 
@@ -248,7 +391,12 @@ from employee
 where dno in (select dno from employee where job = 'MANAGER')
 
 
-
+--
+select ename 사원이름, dno 부서번호
+from employee
+where dno in (select dno
+            from employee
+            where job = 'MANAGER');
 
 
 
---
+++ 7일차/system - 계정으로 접속.sql
@@ -0,0 +1,5 @@
+show user;
+
+-- system (관리자 권한) 에서 HR 계정에게 dba 
+
+grant dba to C##HR;
